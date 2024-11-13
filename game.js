3f07534af342dab0f47380c19f6998b22f03868e
elinaasratyan <asel24mr@student.ju.se>
2024-11-14 00:47:18 +0100

    added savior character in a default position

--- game.js
+++ game.js
@@ -12,8 +12,8 @@ setup();
 
 background(255, 255, 255);
 
-let y = 140;
 let x = 400;
+let y = 140;
 
 function victim(x, y, s) {
   // the code of the victim stickman
@@ -34,9 +34,12 @@ function victim(x, y, s) {
   rect(x - 85 * s, y - 60 * s, 170 * s, 250 * s);
   pop();
 
+  push();
   fill(105, 72, 54);
+  strokeWeight(0);
   quad(x - 85 * s, y + 190 * s, x - 50 * s, y + 150 * s, x + 50 * s, y + 150 * s, x + 85 * s, y + 190 * s);
   quad(x - 85 * s, y - 60 * s, x - 50 * s, y - 20 * s, x + 50 * s, y - 20 * s, x + 85 * s, y - 60 * s);
+  pop();
 
   push();
   strokeWeight(2 * s);
@@ -53,11 +56,10 @@ function victim(x, y, s) {
 
   // THE VICTIM STICKMAN
 
+    // arms
   push();
   fill(0, 0, 0);
-  strokeWeight(5 * s);
-
-  // arms
+  strokeWeight(4 * s);
   line(x, y + 50 * s, x + 60 * s, y - 20 * s);
   ellipse(x + 60 * s, y - 20 * s, 4 * s, 3 * s);
   line(x, y + 50 * s, x - 60 * s, y - 20 * s);
@@ -76,6 +78,7 @@ function victim(x, y, s) {
   pop();
 
   // eyes
+  strokeWeight(2 * s);
   fill(0, 0, 0);
   ellipse(x - 12 * s, y - 4 * s, 7 * s);
   ellipse(x + 12 * s, y - 4 * s, 7 * s);
@@ -84,17 +87,90 @@ function victim(x, y, s) {
   ellipse(x, y + 12 * s, 30 * s, 20 * s);
 }
 
+/*
+let x = 400;
+let y = 140;
+*/
+
+function savior(x, y, s) {
+  // the code for the savior stickman
+
+  // the switch control for the savior
+  push();
+  stroke(150);
+  strokeWeight(3);
+  noFill();
+  triangle(x + 200, y + 595, x + 230, y + 560, x + 260, y + 595);
+  line(x + 230, y + 560, x + 260, y + 530);
+
+  // arm goes behind the switch
+  push();
+  stroke(0);
+  line(x + 260, y + 520, x + 310, y + 495);
+  pop();
+
+  fill(255, 0, 0);
+  stroke(132, 8, 8);
+  ellipse(x + 260, y + 530, 20);
+  pop();
+  
+  // THE SAVIOR STICKMAN
+
+  // arms
+  push();
+  stroke(0, 0, 0);
+  strokeWeight(4 * s);
+
+  // body
+  push();
+  fill(255, 255, 255);
+  translate(x + 320, y + 520);
+  rotate(2.9);
+  rect(-14, -32, 40 * s, 85 * s);
+  pop();
+
+  line(x + 270, y + 530, x + 310, y + 500);
+  ellipse(x + 270, y + 530, 4 * s, 3 * s);
+
+  // head
+
+  fill(255, 255, 255);
+  ellipse(x + 308, y + 478, 60 * s);
+
+
+  // legs
+  line(x + 340, y + 545, x + 350, y + 595);
+  line(x + 313, y + 553, x + 300, y + 575);
+  line(x + 300, y + 575, x + 305, y + 595);
+  ellipse(x + 348, y + 595, 4 * s, 3 * s);
+  ellipse(x + 303, y + 595, 4 * s, 3 * s);
+
+  // eyes
+  strokeWeight(2 * s);
+  fill(0, 0, 0);
+  ellipse(x + 295, y + 475, 7 * s);
+  ellipse(x + 295 + (24 * s), y + 475, 7 * s);
+  
+  // mouth
+  strokeWeight(3);
+  line(x + 295, y + 485, x + 295 + (24 * s), y + 485);
+
+}
+
 let speed = 3;
 
 function draw() {
   background(255, 255, 255);
   victim(x, y, 0.7);
 
-/*
-  if (y <= 800) {
+  savior(400, 140, 0.7);
+
+
+  if (y <= 600) {
     y += 3;
   }
-*/
+
+
 
 /*
     y+=speed;
@@ -103,12 +179,4 @@ function draw() {
         console.log("Bounce");
     }
 */
-}
-
-function savior(x, y) {
-  //the code for the savior stickman
-
-  //the switch control for the savior
-
-
 }
